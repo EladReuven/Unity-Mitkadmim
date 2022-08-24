38c166f3291de9ef23c638970c92d4ab8c2c7ba4
daniel kaarur <danielkaarur@gmail.com>
2022-08-24 22:25:04 +0300

    commiting same again because of error, fixed this time

--- Defuq/Assets/Scripts/Controllers/Camera/cameraFollow.cs
+++ Defuq/Assets/Scripts/Controllers/Camera/cameraFollow.cs
@@ -22,9 +22,17 @@ namespace Controllers.CameraCtrl
             z = cameraPosition.z;//z is camera z axis coordinate
         }
 
-        void LateUpdate()
+        void FixedUpdate()
         {
-            x += (target.transform.position.x - x+ xOffset) * 0.01f * xfollowSpeed;//moves 1%*followSpeed closer towards our target(player) x axis
+            //version 1 doesnt use Start Event
+            //cameraPosition = Camera.main.transform.position;
+            //x = (target.transform.position.x - cameraPosition.x + xOffset) * 0.01f * xfollowSpeed; // 1%*followSpeed closer towards our target(player) x axis
+            //z = (target.transform.position.z - cameraPosition.z - zOffset) * 0.01f * zfollowSpeed; // 1%*followSpeed closer towards our target(player) z axis
+            //Vector3 cameraMoveVector = new Vector3(x, 0, z);//vector that points on the Target(player)
+            //Camera.main.transform.position = (cameraPosition + cameraMoveVector)* Time.timeScale;//   current camera position + the distance and angle we want to move to(cameraMoveVector) -->* timescale incase we want to pause the game(default equals 1)
+
+            //uses Start Event
+            x += (target.transform.position.x - x + xOffset) * 0.01f * xfollowSpeed;//moves 1%*followSpeed closer towards our target(player) x axis
             z += (target.transform.position.z - z - zOffset) * 0.01f * zfollowSpeed;//moves 1%*followSpeed closer towards our target(player) z axis
             cameraPosition = new Vector3(x, cameraPosition.y, z);
             Camera.main.transform.position = cameraPosition * Time.timeScale;//incase we want to pause the game(default equals 1)
