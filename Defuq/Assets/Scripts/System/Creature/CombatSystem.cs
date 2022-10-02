7dbfeff753fab9c643f761339913accf2b34c247
GDIVX <yarin107@gmail.com>
2022-10-02 12:53:42 +0300

    fixed

--- Defuq/Assets/Scripts/System/Creature/CombatSystem.cs
+++ Defuq/Assets/Scripts/System/Creature/CombatSystem.cs
@@ -7,9 +7,9 @@ using UnityEngine;
 public class CombatSystem : MonoBehaviour
 {
 
-    public void TakeDamage(int takenDamage,string creatureType)
+    public void TakeDamage(int takenDamage, string creatureType)
     {
-        
+
         if (creatureType.Equals("Player"))
         {
             if (takenDamage < 0)
@@ -17,36 +17,36 @@ public class CombatSystem : MonoBehaviour
             int playerHealth = GameManager.instance.playerData.GetCurrentHealth();
             playerHealth -= takenDamage;
             if (playerHealth < 0)
-            { 
-                playerHealth = 0;
+            {
+                GameManager.instance.GameOver();
             }
             GameManager.instance.playerData.SetCurrentHealth(playerHealth);
             Debug.Log("<color=red>Player's health: </color><color=green>" + GameManager.instance.playerData.GetCurrentHealth() + "</color>");
         }
-        
+
     }
     public void TakeDamage(int takenDamage, GameObject Enemy, CreatureController _enemyData)
     {
-            int enemyCurrentHealth = _enemyData.GetCurrentHealth();
-            enemyCurrentHealth -= takenDamage;
-            if (enemyCurrentHealth <= 0)
+        int enemyCurrentHealth = _enemyData.GetCurrentHealth();
+        enemyCurrentHealth -= takenDamage;
+        if (enemyCurrentHealth <= 0)
+        {
+            enemyCurrentHealth = 0;
+            _enemyData.GetEnemySwitch().AnimatorKilledTrue();
+            Enemy.GetComponent<CreatureController>().enabled = false;
+
+            _enemyData.GetEnemyEvent().enemyKilled.Invoke();
+            if (gameObject.layer == 7)
             {
-                enemyCurrentHealth = 0;
-                _enemyData.GetEnemySwitch().AnimatorKilledTrue();
-                Enemy.GetComponent<CreatureController>().enabled=false;
-                
-                _enemyData.GetEnemyEvent().enemyKilled.Invoke();
-                if (gameObject.layer == 7)
-                {
-                    GameManager.instance.WinGame();
-                }
-                GameManager.instance.enemiesAlive--;
-                Debug.Log("Enemy Killed");
+                GameManager.instance.WinGame();
             }
-            _enemyData.SetCurrentHealth(enemyCurrentHealth);
-            Debug.Log("<color=red>Enemy's health: </color><color=green>" + _enemyData.GetCurrentHealth()+ "</color>");
+            GameManager.instance.enemiesAlive--;
+            Debug.Log("Enemy Killed");
+        }
+        _enemyData.SetCurrentHealth(enemyCurrentHealth);
+        Debug.Log("<color=red>Enemy's health: </color><color=green>" + _enemyData.GetCurrentHealth() + "</color>");
     }
 
-    
+
 
 }
